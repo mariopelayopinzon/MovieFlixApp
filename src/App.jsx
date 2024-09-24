7303602dd9e3b848ba6cfce1584b014967f7b469
mariopelayopinzon <mariopelayo.psic@gmail.com>
2024-09-23 18:16:49 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Movie from "./components/Movie";
+import SearchBar from "./components/SearchBar";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar></SearchBar>
+    </div>
+  );
+}
+
+export default App;
